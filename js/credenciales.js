ff5c31de57885d3216bbefbb260277a43272e071
Gonzalo Bechara <60068962+cacalo@users.noreply.github.com>
2023-03-07 11:40:21 -0300

    Espacio vacío para credenciales

---
+++ js/credenciales.js
@@ -0,0 +1,4 @@
+// TODO(developer): Set to client ID and API key from the Developer Console
+const CLIENT_ID = 'ENTER OAUTH2 CLIENT ID';
+const API_KEY = 'ENTER API KEY';
+const SPREADSHEET = "ENTER SPREADHSEET ID";
