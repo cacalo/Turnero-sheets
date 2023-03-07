92adf74e80b614ae56765ecf8cfafc8cd031cc14
Gonzalo Bechara <60068962+cacalo@users.noreply.github.com>
2023-03-07 11:38:41 -0300

    Versión original sin variables de entorno

---
+++ js/index.js
@@ -0,0 +1,59 @@
+// import turnos from "./turnos.js";
+
+const turnosContainer = document.getElementById("turnosContainer");
+const detalleContainer = document.getElementById("detalleContainer");
+let indiceSeleccionado;
+
+const clienteElement = document.getElementById("cliente");
+const modeloElement = document.getElementById("modelo");
+const problemaElement = document.getElementById("problema");
+const comentarioElement = document.getElementById("comentario");
+const marcarTerminadoElement = document.getElementById("finalizar");
+
+
+function createTarjeta(turno,index){
+  const nuevaTarjeta = document.createElement("div");
+  nuevaTarjeta.classList = "tarjeta";
+  nuevaTarjeta.innerHTML = `
+    <h3>${turno.cliente}</h3>
+    <p>${turno.email}</p>
+    <p>${turno.modelo}</p>
+    <p>${turno.problema}</p>
+  `
+  nuevaTarjeta.addEventListener("click", ()=> actualizarDetalle(index))
+  turnosContainer.appendChild(nuevaTarjeta);
+}
+
+function actualizarTarjetas(){
+  turnosContainer.innerHTML = "";
+  turnos.forEach((turno,i) => {
+    createTarjeta(turno,i);
+  })
+}
+
+function actualizarDetalle(index){
+  if(indiceSeleccionado !== undefined) turnosContainer.children[indiceSeleccionado].classList.toggle("seleccionado",false);
+  clienteElement.innerText = turnos[index].cliente;
+  modeloElement.innerText = turnos[index].modelo;
+  problemaElement.innerText = turnos[index].problema;
+  detalleContainer.classList.toggle("escondido",false);
+  indiceSeleccionado = index;
+  turnosContainer.children[indiceSeleccionado].classList.toggle("seleccionado",true);
+}
+
+finalizar.addEventListener("click",()=> marcarTerminado(indiceSeleccionado))
+
+async function marcarTerminado(i){
+  const updateTurno = turnos[i];
+  updateTurno.comentario = comentarioElement.value;
+  updateTurno.fecha_terminado = new Date().toISOString;
+  const res = await editTurno(updateTurno.id,updateTurno);
+  if(res.status === 200){
+    turnos = turnos.filter(turno => turno.id !== updateTurno.id);
+    indiceSeleccionado = 0;
+    await actualizarTarjetas()
+    detalleContainer.classList.toggle("escondido",true);
+    comentarioElement.value="";
+  }
+}
+
---
+++ js/sheets.js
@@ -0,0 +1,55 @@
+const hoja = "Turnos";
+let turnos;
+
+async function getTurnos() {
+  let response;
+  try {
+    response = await gapi.client.sheets.spreadsheets.values.get({
+      spreadsheetId: SPREADSHEET,
+      range: hoja + "!A:G",
+    });
+  } catch (err) {
+    document.getElementById("content").innerText = err.message;
+    return;
+  }
+  const range = response.result;
+  if (!range || !range.values || range.values.length == 0) {
+    document.getElementById("content").innerText = "No values found.";
+    return;
+  }
+
+  turnos = [];
+  range.values.forEach((fila) => {
+    if (isNaN(parseInt(fila[0])) || fila[5] !== undefined) return;
+    const nuevoTurno = {
+      id: fila[0],
+      cliente: fila[1],
+      email: fila[2],
+      modelo: fila[3],
+      problema: fila[4],
+      fecha_terminado: fila[5],
+      comentario: fila[6]
+    };
+    turnos.push(nuevoTurno);
+  });
+}
+
+async function editTurno(id, contenido) {
+  const update = [
+    contenido.id,
+    contenido.cliente,
+    contenido.email,
+    contenido.modelo,
+    contenido.problema,
+    new Date().toISOString(),
+    contenido.comentario,
+  ]
+  const filaAEditar = parseInt(id)+1;
+  response = await gapi.client.sheets.spreadsheets.values.update({
+    spreadsheetId: SPREADSHEET,
+    range: `${hoja}!A${filaAEditar}:G${filaAEditar}`,
+    values: [update],
+    valueInputOption:"USER_ENTERED"
+  });
+  return response;
+}
